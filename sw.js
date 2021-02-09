70ca96462d20dd0d28fc3116c9731fc768b17e40
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2021-02-09 10:20:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var CACHE_NAME = 'todo-v1';
-var urlsToCache = [
+car CACHE_URLS = [
   '/todo/',
   '/todo/index.html',
   '/todo/css/theme/theme.css',
@@ -10,14 +10,15 @@ var urlsToCache = [
   '/todo/js/events.js',
   '/todo/js/doms.js',
   '/todo/js/database.js',
+  'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+  'https://fonts.googleapis.com/icon?family=Material+Icons&display=swap'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(CACHE_URLS);
       })
   );
 });

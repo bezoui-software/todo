d0035fb8f31eddb5a90bc0baa17eb693124b9c78
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2021-02-11 22:03:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,12 +4,7 @@ var CACHE_URLS = [
   '/todo/index.html',
   '/todo/css/theme/theme.css',
   '/todo/css/index.css',
-  '/todo/js/main.js',
-  '/todo/js/todos.js',
-  '/todo/js/todo.js',
-  '/todo/js/events.js',
-  '/todo/js/doms.js',
-  '/todo/js/database.js',
+  '/todo/js/main.min.js',
   'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
   'https://fonts.googleapis.com/icon?family=Material+Icons&display=swap'
 ];
